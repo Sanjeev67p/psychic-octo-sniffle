8d5572ed714532ee59df16ebbae9b62b2764a54d
Sanjeev67p <86890546+Sanjeev67p@users.noreply.github.com>
2022-10-10 21:57:33 +0530

    commit

--- Java_prac/p1.java
+++ Java_prac/p1.java
@@ -3,7 +3,9 @@ public class p1{
     public static void main(String[] args){
         System.out.println("Aasad");
         Scanner Sc = new int(System.in);
-        
+        int a = "roht";
+        ins aksms =
+
         
     }
 }
